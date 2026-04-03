a19ce13b8aee42bf00c920ff17e95d5906aec376
ethan <yangethan970503@gmail.com>
2026-04-04 06:58:25 +0900

    Rename winner to svt_av1_lanczos, update to fg22 crf33 (score 2.20)

--- submissions/svt_av1_lanczos/compress.sh
+++ submissions/svt_av1_lanczos/compress.sh
@@ -22,8 +22,9 @@ while IFS= read -r rel; do
   ffmpeg -nostdin -y -hide_banner -loglevel warning \
     -r 20 -fflags +genpts -i "$IN" \
     -vf "scale=trunc(iw*0.45/2)*2:trunc(ih*0.45/2)*2:flags=lanczos" \
-    -c:v libsvtav1 -preset 0 -crf 32 \
+    -c:v libsvtav1 -preset 0 -crf 33 \
     -g 180 \
+    -svtav1-params "film-grain=22:film-grain-denoise=1" \
     -r 20 "$OUT"
 done < "$VIDEO_NAMES_FILE"
 
